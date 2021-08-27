c1b06d4e04d20bf0ab716689fcc93854230709d9
ciottisimone88 <ciottisimone88@gmail.com>
2021-08-27 10:59:48 +0200

    Update cartesian_pose_controller_cino.h

--- include/panda_controllers/cartesian_pose_controller_cino.h
+++ include/panda_controllers/cartesian_pose_controller_cino.h
@@ -14,6 +14,25 @@
 
 #include <franka_hw/franka_cartesian_command_interface.h>
 
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <controller_interface/multi_interface_controller.h>
+#include <dynamic_reconfigure/server.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Vector3Stamped.h>
+#include <std_msgs/Float64.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+#include <eigen3/Eigen/Dense>
+
+#include <franka_example_controllers/compliance_paramConfig.h>
+#include <franka_hw/franka_model_interface.h>
+#include <franka_hw/franka_state_interface.h>
+
 namespace panda_controllers {
 
 class CartesianPoseControllerCino
